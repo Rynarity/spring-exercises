e7d224bfa8b9a7d2a89d0c7c73dc0d3b658d15d1
Rynarity <aryann.magluyan@gmail.com>
2023-02-07 04:29:04 +0800

    add get and save student on controller level only

--- 09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ 09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -1,11 +1,23 @@
 package com.ltp.gradesubmission.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ltp.gradesubmission.entity.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable Long id) {
+        return new ResponseEntity<Student>(HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
+        return new ResponseEntity<Student>(student, HttpStatus.CREATED);
+    }
 
 }

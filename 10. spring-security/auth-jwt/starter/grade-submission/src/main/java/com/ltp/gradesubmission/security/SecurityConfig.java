7af4c3c0a413d0daeb7936f5621b357afed6e62d
Rynarity <aryann.magluyan@gmail.com>
2023-10-03 16:36:58 +0800

    add filters for handling incorrect json fields/other errors for the meantime

--- 10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
+++ 10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.ltp.gradesubmission.security;
 
 
+import com.ltp.gradesubmission.security.filter.AuthenticationFilter;
+import com.ltp.gradesubmission.security.filter.ExceptionHandlerFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import lombok.AllArgsConstructor;
-
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static com.ltp.gradesubmission.security.SecurityConstants.REGISTER_PATH;
 
 
 @Configuration
@@ -16,16 +20,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http        
-            .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
-            .and()
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
-            .anyRequest().authenticated()
-            .and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setFilterProcessesUrl("/authenticate");
+        http
+                .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, REGISTER_PATH).permitAll() // Any requests made to the register path will not require authentication
+                .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class) // Add filter before the authentication. Will just wrap a try catch to the doFilter (AuthenticationFilter) and if it has any error, will return an error 400 status code.
+                .addFilter(authenticationFilter)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
-    
+
 }
--- 10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
+++ 10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
@@ -1,5 +1,28 @@
 package com.ltp.gradesubmission.security.filter;
 
-public class AuthenticationFilter {
-    
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ltp.gradesubmission.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            // Surrounded with Try-Catch so that if the json request field does not match with our Entity columns in User
+            // It will throw a RunTimeException
+            System.out.println(user.getUsername());
+            System.out.println(user.getPassword());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return super.attemptAuthentication(request, response);
+    }
 }

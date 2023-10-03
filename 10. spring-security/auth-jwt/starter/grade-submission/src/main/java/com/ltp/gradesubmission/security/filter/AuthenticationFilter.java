b295769b66c9ba9437b64e0688b924328f0710d1
Rynarity <aryann.magluyan@gmail.com>
2023-10-03 23:05:58 +0800

    Handle creation of JWT token for successful auth and 401 response for unsuccessful auth

--- 10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
+++ 10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
@@ -1,8 +1,10 @@
 package com.ltp.gradesubmission.security.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ltp.gradesubmission.entity.User;
-import com.ltp.gradesubmission.security.manager.CustomAuthenticationManager;
+import com.ltp.gradesubmission.security.SecurityConstants;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,6 +18,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @AllArgsConstructor
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -43,14 +46,20 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-//        super.successfulAuthentication(request, response, chain, authResult);
+        String token = JWT.create()
+                .withSubject(authResult.getName())
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.TOKEN_EXPIRATION))
+                .sign(Algorithm.HMAC512(SecurityConstants.SECRET_KEY));
+        response.addHeader(SecurityConstants.AUTHORIZATION, SecurityConstants.BEARER + token);
         System.out.println("IT WORKED!!!!");
     }
 
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-//        super.unsuccessfulAuthentication(request, response, failed);
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.getWriter().write(failed.getLocalizedMessage());
+        response.getWriter().flush();
         System.out.println("Failed");
     }
 }
